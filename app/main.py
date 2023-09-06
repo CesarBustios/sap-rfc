060b964ada8487c7f420182d2f536c12600d29e1
César Bustíos <cbustios@gocatalant.com>
2023-09-06 18:15:20 -0500

    Pre-commit

--- app/main.py
+++ app/main.py
@@ -1,13 +1,25 @@
 import json
 
 import falcon
+from pyrfc import Connection
 
 
 class HealthResource:
-    def on_get(self, req, resp):
+    def on_get(self, request, response):
         """Handles GET requests"""
-        resp.status = falcon.HTTP_200
-        resp.text = json.dumps({"health": "OK"}, ensure_ascii=False)
+        response.status = falcon.HTTP_200
+        response.text = json.dumps({"health": "OK"}, ensure_ascii=False)
+
+
+class RFCResource:
+    def on_get(self, request, response):
+        """Handles GET requests"""
+        with Connection(
+            ashost="10.0.0.1", sysnr="00", client="100", user="me", passwd="secret"
+        ) as conn:
+            print(conn)
+        response.status = falcon.HTTP_200
+        response.text = json.dumps({}, ensure_ascii=False)
 
 
 # falcon.App instances are callable WSGI apps
